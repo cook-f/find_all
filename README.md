0d2c349e59e7e61fc91c2b864f94045568dc0d7d
nectariner <fredcook789@protonmail.com>
2022-06-29 18:57:10 +0100

    working 2.0

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # find_all
 
-`find_all` is capable of finding all indexes of elements where a predicate is met and therefore aims to be a simple alternative with  (nearly) identical interface to the `find` method (this difference being returning an `Option<Vec<usize>>` instead of `Option<usize>`)
+`find_all` is capable of finding all indexes of elements where a given predicate is met and therefore aims to be a simple alternative with  (nearly) identical interface to the `find` method (this difference being returning an `Option<Vec<usize>>` instead of `Option<usize>`)
 
 
 ```rust
+use find_all::FindAll;
 let test_data = [1, 2, 3, 4, 1, 1, 1, 1];
-let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 9);
+let indexes = test_data.iter().find_all(|num: &&i32| **num == 9);
 assert_eq!(indexes, None);
 
-let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 1);
+let indexes = test_data.iter().find_all(|num: &&i32| **num == 1);
 assert_eq!(indexes, Some(vec![0,4,5,6,7]));
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,43 @@
-//! `find_all` is capable of finding all indexes of elements where a predicate is met and therefore aims to be a simple alternative with  (nearly) identical interface to the `find` method (this difference being returning an `Option<Vec<usize>>` instead of `Option<usize>`)
+//! `find_all` is capable of finding all indexes of elements where a given predicate is met and therefore aims to be a simple alternative with  (nearly) identical interface to the `find` method (this difference being returning an `Option<Vec<usize>>` instead of `Option<usize>`)
 //!
 //!
 //! ```rust
+//! use find_all::FindAll;
 //! let test_data = [1, 2, 3, 4, 1, 1, 1, 1];
-//! let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 9);
+//! let indexes = test_data.iter().find_all(|num: &&i32| **num == 9);
 //! assert_eq!(indexes, None);
 //!
-//! let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 1);
+//! let indexes = test_data.iter().find_all(|num: &&i32| **num == 1);
 //! assert_eq!(indexes, Some(vec![0,4,5,6,7]));
 //! ```
 
 /// ```rust
-/// let test_data = vec![1, 2, 3, 4, 1, 1, 1, 1];
-/// let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 1);
-/// assert_eq!(indexes, Some(Vec::from([0, 4, 5, 6, 7])));
+/// use find_all::FindAll;
+///let test_data = vec![1, 2, 3, 4, 1, 1, 1, 1];
 ///
-/// let test_data = vec![1, 2, 3, 4, 1, 1, 1, 1];
-/// let indexes = find_all::find_all(test_data.iter(), |num: &i32| *num == 9);
-/// assert_eq!(indexes, None);
+///let indexes = test_data.iter().find_all(|num: &&i32| **num == 1);
+///assert_eq!(indexes, Some(Vec::from([0, 4, 5, 6, 7])));
 ///
-/// let test_data = vec![
-///     String::from("hello"),
-///     String::from("goodbye"),
-///     String::from("hello"),
-/// ];
-/// let indexes = find_all::find_all(test_data.iter(), |string| string.contains('o'));
-/// assert_eq!(indexes, Some(vec![0, 1, 2]));
+///let test_data = vec![1, 2, 3, 4, 1, 1, 1, 1];
+///let indexes = test_data.iter().find_all(|num| **num == 9);
+///assert_eq!(indexes, None);
+///
+///let test_data = vec![
+///    String::from("hello"),
+///    String::from("goodbye"),
+///    String::from("hello"),
+///];
+///let indexes = test_data.iter().find_all(|string| string.contains('o'));
+///assert_eq!(indexes, Some(vec![0, 1, 2]));
 /// ```
-pub trait IteratorExt: Iterator + Sized {
+
+pub trait FindAll: Iterator + Sized {
     fn find_all<P>(&mut self, predicate: P) -> Option<Vec<usize>>
     where
         P: FnMut(&Self::Item) -> bool;
 }
 
-impl<I> IteratorExt for I
+impl<I> FindAll for I
 where
     I: Iterator,
 {
